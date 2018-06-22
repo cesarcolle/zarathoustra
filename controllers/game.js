05ddc3ad9a155c721486bf3bdd43708059103416
César Collé <cesar.colle@gmail.com>
2018-06-22 21:05:05 +0200

    Add the URL integration with BoardGameGeek API for more information on games.

--- controllers/game.js
+++ controllers/game.js
@@ -1,7 +1,34 @@
-require("http")
-const boardGeekAPI = process.env.BOARDGEEK_URL
+const https = require("https");
+const boardGeekAPI = process.env.BOARDGEEK_URL;
+const concat = require("concat-stream");
 const Game = require('../models/Game');
 
+
+function getBoardGeekGameXmlContent(name) {
+    const https = require('https');
+
+    const options = {
+        hostname: 'boardgamegeek.com',
+        port: 443,
+        path: "/xmlapi/search?search=" + name,
+        method: 'GET'
+    };
+
+    return https.get(options, (res) => {
+        console.log('statusCode:', res.statusCode);
+        console.log('headers:', res.headers);
+        var output = '';
+        res.on('data', (chunk) => {
+            output += chunk
+        });
+        res.on('end', function() {
+            return output;
+        });
+
+    }).on('error', (e) => {
+        console.error(e);
+    });
+}
 
 exports.getGames = (req, res) => {
     res.render('game', {
@@ -11,14 +38,13 @@ exports.getGames = (req, res) => {
 
 
 exports.addGame = (req, res) => {
-    console.log("add game controller");
+    boardDescription = getBoardGeekGameXmlContent(req.body.name);
+    console.log("RETURN :: "  + boardDescription.toString())
     const gameCreated = new Game({
-        info : {
-            name : req.nameGame,
-            price : req.price
+        info: {
+            name: req.body.nameGame,
+            price: req.body.price
         }
-    });
-    console.log(gameCreated.find({name : a }, callback));
-     
-    req.flash('success', { msg: 'You add properly the new game !' });
+    }).save();
+    req.flash('success', {msg: 'You add properly the new game !'});
 };
